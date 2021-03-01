2b402b0ee519205fd8df487e62a4f37f16a58aec
H34dl3ss <57181552+H34dl3ss@users.noreply.github.com>
2021-03-01 12:38:38 +0100

    Update unpack_usnf.bat

--- unpack_usnf.bat
+++ unpack_usnf.bat
@@ -1,11 +1,11 @@
 REM 26.12.2020, H34dl3ss
-REM A script to unpack Archives from usenet on a local PC running Windows.
+REM This is a batch script to unpack archives from usenet on a local PC running Windows.
 REM The directory containing the rar files should look like [directory{{password}}etc]
-REM The password will be extracted and the content of the directory will be extracted
-REM to [directory].
+REM The password will be extracted and the content of the directory will be extracted to [directory].
 REM The batch file shall be placed in the same directory as the directories to be unpacked.
 REM Make sure to modify the path to your local WinRAR.exe in the line "set unrar=..."
 REM If another target directory is preferred, change "set scriptdir=..." to a path of choice.
+REM Processed directories are moved to the folder "!done" which can be set differently also.
 REM
 REM Caution! Limited errorhandling! Use at own risk!
 REM
@@ -13,6 +13,7 @@ REM
 @echo off
 cls
 set scriptdir=%~dp0
+set donedir=%~dp0\^!done
 set unrar=C:\Program\WinRAR\WinRAR.exe x -p
 setlocal enabledelayedexpansion
 
@@ -34,8 +35,8 @@ for /D %%i IN (*) DO (
 	echo Target: 	!scriptdir!!newdir!
 	set exec=!unrar!!pw! "!var!\*1.rar" "!scriptdir!!newdir!\"
 	call !exec!
-        if not exist "!scriptdir!"\done\ mkdir "!scriptdir!"\done\
-        move "!var!" "!scriptdir!"\done\
+        if not exist "!donedir!"\ mkdir "!donedir!"\
+        move "!var!" "!donedir!"\
         echo done
 	echo.
 	echo.
