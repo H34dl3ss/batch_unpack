41ed4812b4a618ff4297d0c5ef124fa8a720407b
H34dl3ss <57181552+H34dl3ss@users.noreply.github.com>
2022-02-28 17:02:52 +0100

    Update unpack_usnf.bat
    Added 7zip as alternative extractor.

--- unpack_usnf.bat
+++ unpack_usnf.bat
@@ -1,9 +1,11 @@
 REM 26.12.2020, H34dl3ss
+REM 28.02.2022, H34dl3ss Update: add 7zip as extractor
 REM This is a batch script to unpack archives from usenet on a local PC running Windows.
 REM The directory containing the rar files should look like [directory{{password}}etc]
 REM The password will be extracted and the content of the directory will be extracted to [directory].
 REM The batch file shall be placed in the same directory as the directories to be unpacked.
 REM Make sure to modify the path to your local WinRAR.exe in the line "set unrar=..."
+REM To use 7zip, enter the path to the 7z.exe and set "use7zip" to yes.
 REM If another target directory is preferred, change "set scriptdir=..." to a path of choice.
 REM Processed directories are moved to the folder "!done" which can be set differently also.
 REM
@@ -15,6 +17,8 @@ cls
 set scriptdir=%~dp0
 set donedir=%~dp0\^!done
 set unrar=C:\Program\WinRAR\WinRAR.exe x -p
+set unzip="C:\Program Files\7zip\7z.exe" x -ba
+set use7zip="yes"
 setlocal enabledelayedexpansion
 
 for /D %%i IN (*) DO (
@@ -33,7 +37,12 @@ for /D %%i IN (*) DO (
 	echo Source: 	!var! 
 	echo Password: 	!pw!
 	echo Target: 	!scriptdir!!newdir!
-	set exec=!unrar!!pw! "!var!\*1.rar" "!scriptdir!!newdir!\"
+	echo.        
+	     IF !use7zip!=="yes" (
+             set exec=!unzip! "!var!\*.rar" -p!pw! -o"!scriptdir!!newdir!\"
+             ) ELSE (
+             set exec=!unrar!!pw! "!var!\*.rar" "!scriptdir!!newdir!\"
+             )
 	call !exec!
         if not exist "!donedir!"\ mkdir "!donedir!"\
         move "!var!" "!donedir!"\
